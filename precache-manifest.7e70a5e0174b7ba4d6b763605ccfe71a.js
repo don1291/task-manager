4dd4cd04186747b9f30ee51a0c03fca299dded3a
don1291 <don1291@mac.mynetworksettings.com>
2025-01-03 15:22:38 -0500

    Updates

---
+++ precache-manifest.7e70a5e0174b7ba4d6b763605ccfe71a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5344df716362fddfeee237d36f594e6",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "461cdfcf66a942f9d816",
+    "url": "/task-manager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a46bd5c4ff79f64eaef0",
+    "url": "/task-manager/static/js/2.5da04739.chunk.js"
+  },
+  {
+    "revision": "d17bf58ce29c21b922f6",
+    "url": "/task-manager/static/js/3.d2763252.chunk.js"
+  },
+  {
+    "revision": "461cdfcf66a942f9d816",
+    "url": "/task-manager/static/js/main.a4850fe6.chunk.js"
+  },
+  {
+    "revision": "465a986df950c207b47c",
+    "url": "/task-manager/static/js/runtime~main.25ab7515.js"
+  }
+]);
